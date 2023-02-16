cf1228266d7f3228854b2a4599a41f6c679dac4b
Огай Елизавета <i61puff@gmail.com>
2023-02-17 01:47:19 +0300

    4

--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -4,6 +4,7 @@ from infra_app import views
 app_name = 'infra_app'
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('second_page/', views.second_page, name='second_page'),
 
 ]
