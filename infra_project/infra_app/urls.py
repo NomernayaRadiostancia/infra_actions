3f1589c7c44daf912181ab3ccdaa55463edb302f
Огай Елизавета <i61puff@gmail.com>
2023-02-17 01:41:38 +0300

    2

--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from infra_app import views
 
 app_name = 'infra_app'
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
     path('second_page/', views.second_page, name='second_page'),
 
 ]
